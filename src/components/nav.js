230200cc499bcc05acc1759a9925c93a4bf4f5b5
Sayak Bhattacharjee <sayak718.s@gmail.com>
2022-05-23 05:25:44 +0530

    navbar added

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import react from "react"
+import "../styles/nav.css"
+export default function NavBar(){
+    return(
+      <div className="container">
+        <header>
+          <h1 className="title">Savings.com</h1>
+          <nav>  
+            <a className="nav-el" href="#">Home</a>
+            <a className="nav-el" href="#">About</a>
+          </nav>
+        </header>  
+      </div>
+    )
+}
